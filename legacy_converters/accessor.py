434beecad72ae75b5e2b8300c2d7d66cc78f2026
Justus Magin <keewis@posteo.de>
2025-10-30 21:01:04 +0100

    move `maybe_convert` into `legacy_converters.crs`

--- legacy_converters/accessor.py
+++ legacy_converters/accessor.py
@@ -5,31 +5,7 @@ from functools import cached_property
 import pyproj
 import xarray as xr
 
-from legacy_converters.crs import CRSLike, create_transformer
-
-
-def maybe_convert(
-    path: str, ds: xr.Dataset, transformer: pyproj.Transformer
-) -> xr.Dataset:
-    if not {"x", "y"}.issubset(ds.dims):
-        # no spatial dims
-        return ds
-
-    xx, yy = xr.broadcast(ds["x"], ds["x"])
-
-    axis_names = [ax.abbrev.lower() for ax in transformer.target_crs.axis_info]
-    axis_attrs = transformer.target_crs.cs_to_cf()
-
-    converted = xr.apply_ufunc(
-        transformer.transform, xx, yy, output_core_dims=[[]] * len(axis_names)
-    )
-
-    new_coords = {
-        name: coord.assign_attrs(attrs)
-        for name, coord, attrs in zip(axis_names, converted, axis_attrs)
-    }
-
-    return ds.assign_coords(new_coords)
+from legacy_converters.crs import CRSLike, create_transformer, maybe_convert
 
 
 @xr.register_datatree_accessor("grid4earth")
--- legacy_converters/crs.py
+++ legacy_converters/crs.py
@@ -1,4 +1,5 @@
 import pyproj
+import xarray as xr
 
 CRSLike = int | str | pyproj.CRS
 
@@ -18,3 +19,27 @@ def create_transformer(source_crs: CRSLike, target_crs: CRSLike) -> pyproj.Trans
     return pyproj.Transformer.from_crs(
         ensure_crs(source_crs), ensure_crs(target_crs), always_xy=True
     )
+
+
+def maybe_convert(
+    path: str, ds: xr.Dataset, transformer: pyproj.Transformer
+) -> xr.Dataset:
+    if not {"x", "y"}.issubset(ds.dims):
+        # no spatial dims
+        return ds
+
+    xx, yy = xr.broadcast(ds["x"], ds["x"])
+
+    axis_names = [ax.abbrev.lower() for ax in transformer.target_crs.axis_info]
+    axis_attrs = transformer.target_crs.cs_to_cf()
+
+    converted = xr.apply_ufunc(
+        transformer.transform, xx, yy, output_core_dims=[[]] * len(axis_names)
+    )
+
+    new_coords = {
+        name: coord.assign_attrs(attrs)
+        for name, coord, attrs in zip(axis_names, converted, axis_attrs)
+    }
+
+    return ds.assign_coords(new_coords)

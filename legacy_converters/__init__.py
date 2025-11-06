0551778df654b2c79b5582553834202ee52fb245
Justus Magin <keewis@posteo.de>
2025-11-06 16:52:23 +0100

    expose the interpolation module

--- legacy_converters/__init__.py
+++ legacy_converters/__init__.py
@@ -1,5 +1,6 @@
 import importlib.metadata
 
+from legacy_converters import interpolation  # noqa: F401
 from legacy_converters.accessor import (  # noqa: F401
     DatasetConverterAccessor,
     DataTreeConverterAccessor,

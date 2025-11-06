bb6b778c65c402f6b4896e0bf450004362d67c5f
Justus Magin <keewis@posteo.de>
2025-11-06 16:53:28 +0100

    expose `bilinear_affine`

--- legacy_converters/interpolation/__init__.py
+++ legacy_converters/interpolation/__init__.py
@@ -1,3 +1,3 @@
-from legacy_converters.interpolation.weights import nearest_affine
+from legacy_converters.interpolation.weights import bilinear_affine, nearest_affine
 
-__all__ = ["nearest_affine"]
+__all__ = ["nearest_affine", "bilinear_affine"]
